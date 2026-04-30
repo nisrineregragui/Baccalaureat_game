8be1639d3ad92b1558f8eb1edca36f65f416871a
kenzaez <kezzahed9@outlook.com>
2026-04-30 14:38:15 +0100

    integrated Groq AI using llama-3.1-8b-instant model for real-time validation + local search in DB

--- src/main/java/com/example/java_project/HelloApplication.java
+++ src/main/java/com/example/java_project/HelloApplication.java
@@ -6,6 +6,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import services.APIService;
+import services.ValidationService;
 
 import java.io.IOException;
 import java.sql.Connection;
@@ -34,6 +36,27 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch();
+        //test API
+        APIService api = new APIService();
+        System.out.println("Test Nisrine/Nom/N : " + api.verify_word("nisrine", "nompropre", 'n'));
+        System.out.println("Test Banane/Animal/B : " + api.verify_word("Banane", "Animal", 'B'));
+
+        //validation test
+        ValidationService validationService = new ValidationService();
+        System.out.println("--- test validatiion ---");
+        //id= 1 ---------------> VILLE ---> true + insert in db
+        boolean test1 = validationService.word_validation("Casablanca", 'c', 1);
+        long startTime = System.currentTimeMillis();
+        long endTime = System.currentTimeMillis();
+        System.out.println("test 1(Casablanca/Ville/C) : " + (test1 ? "RÉUSSI" : "ÉCHEC ") + " en " + (endTime - startTime) + "ms");
+        //test 2 false
+        boolean test2 = validationService.word_validation("Lyon", 'P', 1);
+        System.out.println("test2(Lyon/Ville/P) : " + (!test2 ? "RÉUSSI (Refusé car mauvaise lettre) " : "ÉCHEC "));
+
+        //relancer le test sur "Paris"
+        boolean test3 = validationService.word_validation("Paris", 'P', 1);
+        System.out.println("test 3(SQL) : " + (test3 ? "RÉUSSI " : "ÉCHEC ") + " en " + (endTime - startTime) + "ms");
+        System.out.println("--- FIN DES TESTS ---");
 
     }
 }
