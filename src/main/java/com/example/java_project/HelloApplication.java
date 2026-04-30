3db53c42a4123f6ecefd92073ca81998c5888690
kenzaez <kezzahed9@outlook.com>
2026-04-30 14:38:15 +0100

    Connetion to Database FIXED

--- src/main/java/com/example/java_project/HelloApplication.java
+++ src/main/java/com/example/java_project/HelloApplication.java
@@ -1,11 +1,13 @@
 package com.example.java_project;
 
+import config.DatabaseConnection;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
 
 public class HelloApplication extends Application {
     @Override
@@ -15,6 +17,18 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+        try {
+            System.out.println("Tentative de connexion...");
+            Connection testConn = DatabaseConnection.getConnection();
+
+            if (testConn != null && !testConn.isClosed()) {
+                System.out.println("✅ SUCCÈS : La connexion à MySQL est établie !");
+            }
+        } catch (Exception e) {
+            System.err.println("❌ ÉCHEC : Impossible de se connecter.");
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
